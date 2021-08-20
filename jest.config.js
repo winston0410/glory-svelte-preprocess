9784b5c329c608febd3dae1bbe88f5e19a9c2f1d
kghugo <hugosum.dev@protonmail.com>
2021-08-20 14:57:43 +0100

    Fix jest test path

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,7 @@
 export default {
-  "roots": [
-    "<rootDir>/src"
-  ],
-  "testMatch": [
-    "**/test/**/*.+(ts|tsx|js)",
-    "**/?(*.)+(spec|test).+(ts|tsx|js)"
-  ],
-  "transform": {
-    "^.+\\.(ts|tsx)$": "ts-jest"
+  roots: ["<rootDir>"],
+  testMatch: ["**/test/*.+(spec|test).+(ts|tsx|js)"],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
   },
-}
+};
