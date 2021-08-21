b67561dcbbda2231879a990c5faa0606665e53fa
kghugo <hugosum.dev@protonmail.com>
2021-08-21 17:03:26 +0100

    Add test case for handling descendant combinator in HTML

--- test/issue2.spec.js
+++ test/issue2.spec.js
@@ -1,4 +1,5 @@
 import createTokenizer from "../src/tokenizer";
+import createTransformer from "../src/transformer.js";
 import { getProxiedObject } from "../src/helper";
 import { parse } from "svelte/compiler";
 import path from "path";
@@ -32,6 +33,46 @@ describe("when given a rule with descendant combinator", function () {
           },
         },
       })
+    );
+  });
+});
+
+describe("when given a rule with descendant combinator", function () {
+  it("should add class to the right HTML tag", function () {
+    const code = `
+<style>
+.main .h1 {
+  color: #ff3e00;
+}
+</style><main class="main"><h1 class="h1"></h1></main>`;
+
+    const filename = "/src/routes/index.svelte";
+
+    const classCache = getProxiedObject();
+    const declarationCache = getProxiedObject();
+
+    const tokenizer = createTokenizer(classCache, declarationCache);
+    const ast = parse(code, { filename });
+    const parsedPath = path.parse(filename);
+    tokenizer.generateToken(ast.css, parsedPath);
+
+    const transformer = createTransformer(code, parsedPath).transformHtml(
+      ast.html,
+      classCache
+    )
+    
+    const result = transformer.toString();
+
+    expect(result.replace(/\s/g, "")).toBe(
+      `<style>
+        .main .h1 {
+          color: #ff3e00;
+        }
+      </style>
+      <main>
+          <h1 class="a"></h1>
+      </main>
+`.replace(/\s/g, "")
     );
   });
 });
