014c2898bbb63042cf6e2ab87903d7dd20eecfff
kghugo <hugosum.dev@protonmail.com>
2021-08-25 10:31:42 +0100

    Add test for ignoring svelte block

---
+++ test/conflict.spec.js
@@ -0,0 +1,35 @@
+import wrappedPreprocessor from "./wrapper.js";
+
+describe("when given multiple rules for the same element", function () {
+  it("should add class to the correct tag", function () {
+    const code = `
+<style>
+div {
+  color: red;
+}
+
+.hello{
+  font-size: 20px;
+}
+</style><div class="hello"></div>`;
+
+    const filename = "/src/routes/index.svelte";
+
+    const result = wrappedPreprocessor(code, filename).code;
+
+    expect(result.replace(/\s/g, "")).toBe(
+      `<style>
+        :global(.a){
+          color: red;
+        }
+          
+        :global(.b){
+          font-size: 20px;
+        }
+      </style>
+      <div class="a b">
+      </div>
+`.replace(/\s/g, "")
+    );
+  });
+});
