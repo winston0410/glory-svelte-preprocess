f2a12c074907d921d1f2b94cb8f95fdac42a395b
kghugo <hugosum.dev@protonmail.com>
2021-08-30 22:25:49 +0100

    Add test for overwriting specific class

---
+++ test/atomic.spec.js
@@ -0,0 +1,40 @@
+import wrappedPreprocessor from "./wrapper.js";
+import { reset } from "../src/index.js"
+
+afterEach(() => {
+    reset()
+})
+
+describe("when transforming HTML", function () {
+  it("should only overwrite minified class", function () {
+    const code = `
+<style>
+div {
+  color: red;
+}
+
+.hello{
+  font-size: 20px;
+}
+</style><div class="hello unrelated"></div>`;
+
+    const filename = "/src/routes/index.svelte";
+
+    const result = wrappedPreprocessor(code, filename).code;
+
+    expect(result.replace(/\s/g, "")).toBe(
+      `<style>
+        :global(.a){
+          color: red;
+        }
+          
+        :global(.b){
+          font-size: 20px;
+        }
+      </style>
+      <div class="a b unrelated">
+      </div>
+`.replace(/\s/g, "")
+    );
+  });
+});
