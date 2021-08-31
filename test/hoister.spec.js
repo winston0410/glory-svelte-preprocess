f375d969592fc28a744214f96f0e9dec6b0583f2
kghugo <hugosum.dev@protonmail.com>
2021-08-31 20:41:21 +0100

    Add test for lazy load

--- test/hoister.spec.js
+++ test/hoister.spec.js
@@ -1,6 +1,54 @@
 import wrappedPreprocessor from "./wrapper.js";
 import { reset } from "../src/index.js";
 
+describe("when lazy load is disabled", function () {
+  const opts = {
+    lazyLoad: false,
+  };
+
+  const code = `
+<style>
+.hello{
+  font-size: 20px;
+  color: red;
+}
+</style><div class="hello"></div>`;
+
+  const filename = "/src/routes/index.svelte";
+
+  const layoutCode = `
+<style>
+div {
+    color: red;
+}
+</style>
+<slot></slot>`;
+
+  const layoutFilename = "/src/routes/__layout.svelte";
+
+  it("should hoist all rules to layout.svelte", function () {
+    const result = wrappedPreprocessor(layoutCode, layoutFilename, opts).code;
+    expect(result.replace(/\s/g, "")).toBe(
+      `<style>
+        :global(.a){
+          color: red;
+        }
+        :global(.b){
+          font-size: 20px;
+        }
+      </style><slot></slot>
+`.replace(/\s/g, "")
+    );
+  });
+
+  it("should remove all rules in page", function () {
+    const result = wrappedPreprocessor(code, filename, opts).code;
+    expect(result.replace(/\s/g, "")).toBe(
+      `<style></style><div class="b a"></div>`.replace(/\s/g, "")
+    );
+  });
+});
+
 describe("when an declaration is found in both layout and pages", function () {
   const code = `
 <style>
@@ -23,7 +71,7 @@ div {
   const layoutFilename = "/src/routes/__layout.svelte";
 
   wrappedPreprocessor(layoutCode, layoutFilename).code;
-  
+
   const result = wrappedPreprocessor(code, filename).code;
   it("should hoist that declaration to layout.svelte", function () {
     expect(result.replace(/\s/g, "")).toBe(
@@ -39,10 +87,8 @@ div {
   });
 });
 
-describe('when a declaration is found in all pages', () => {
-    describe('when that declaration is not found in layout', () => {
-        it('should be hoisted to __layout.svelte', async () => {
-            
-        })
-    })
-})
+describe("when a declaration is found in all pages", () => {
+  describe("when that declaration is not found in layout", () => {
+    it("should be hoisted to __layout.svelte", async () => {});
+  });
+});
--- test/wrapper.js
+++ test/wrapper.js
@@ -1,7 +1,7 @@
 import gloryPreprocessor from "../src/index.js";
 
-export default function(content, filename){
-  const { markup } = gloryPreprocessor();
+export default function(content, filename, opts){
+  const { markup } = gloryPreprocessor(opts);
   const result = markup({ content: content, filename: filename });
   return result
 };
